b7dfce2078004b33595dfd204f77cb25427c494b
Nitish Kumar <111587343+nitissk@users.noreply.github.com>
2024-06-22 19:48:20 +0530

    Add files via upload

---
+++ script/login_page.js
@@ -0,0 +1,15 @@
+function test() {
+    var use = document.querySelector('#user').value;
+    var passwor = document.querySelector('#pas').value;
+    if (passwor == '1234' && use == 'nitish') {
+        window.open(
+            "seller.html",
+            "_self"
+        );
+        alert("Login Successful, Welcome to Seller Panel")
+    }
+
+    else {
+        alert("Wrong Username or Password");
+    }
+}
